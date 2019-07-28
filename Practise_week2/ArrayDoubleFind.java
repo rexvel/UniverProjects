e4a31cc496ade49876af5f3cf790513f6cd94c24
Rexvel <rexvelele@gmail.com>
2019-07-28 17:44:18 +0300

    Second week of Applied algorithms

---
+++ Practise_week2/ArrayDoubleFind.java
@@ -0,0 +1,36 @@
+package week2;
+
+public class ArrayDoubleFind {
+
+
+
+        public static void main (String[]args){
+
+
+        int maxSize = 100; // Размер массива
+        HighArray arr; // Ссылка на массив
+        arr = new HighArray(maxSize); // Создание массива
+        arr.insert(77); // Вставка 10 элементов
+        arr.insert(99);
+        arr.insert(44);
+        arr.insert(55);
+        arr.insert(22);
+        arr.insert(88);
+        arr.insert(11);
+        arr.insert(00);
+        arr.insert(66);
+        arr.insert(33);
+        int searchKey = 12; // Поиск элемента
+        if (arr.find(searchKey) != arr.size())
+            System.out.println("Found " + searchKey);
+        else
+            System.out.println("Can't find " + searchKey);
+        arr.display(); // Вывод содержимого
+        arr.delete(00); // Удаление трех элементов
+        arr.delete(55);
+        arr.delete(99);
+        arr.display(); // Повторный вывод
+    }
+
+    }
+
